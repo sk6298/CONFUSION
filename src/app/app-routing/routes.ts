c2be851d89ff041548767f413be90207a55ae3de
Shubham Kabra <shubhamkabra6298@gmail.com>
2020-08-09 22:26:12 +0530

    Single Page Applications Part 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,6 +11,7 @@ export const routes:Routes = [
     {path:'home', component:HomeComponent},
     {path:'menu', component:MenuComponent},
     {path:'about', component:AboutComponent},
+    {path:'dishdetail/:id', component:DishdetailComponent},
     {path:'contactus', component:ContactComponent},
 
 ];
