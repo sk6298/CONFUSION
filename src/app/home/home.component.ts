dd5f0f8dda0a70fd83ca2e46dd14ba890299cc13
Shubham Kabra <shubhamkabra6298@gmail.com>
2020-08-29 14:50:09 +0530

    Promise Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,20 +14,29 @@ import { LeaderService } from '../services/leader.service';
 })
 export class HomeComponent implements OnInit {
 
-  dish:Dish;
-  promotion:Promotion;
-  leader:Leader;
+  dish: Dish;
+  promotion: Promotion;
+  leader: Leader;
 
   constructor(private dishService: DishService,
-     private promotionService: PromotionService,
-     private leaderService: LeaderService) {
-    
+    private promotionService: PromotionService,
+    private leaderService: LeaderService) {
+
   }
 
   ngOnInit() {
-    this.leader = this.leaderService.getFeaturedLeader();
-    this.dish = this.dishService.getFeaturedDish();
-    this.promotion = this.promotionService.getFeaturedPromotion();
+    this.leaderService.getFeaturedLeader()
+      .then((leader) => {
+        this.leader = leader;
+      });
+    this.dishService.getFeaturedDish()
+      .then((dish) => {
+        this.dish = dish;
+      });
+    this.promotionService.getFeaturedPromotion()
+      .then((promotion) => {
+        this.promotion = promotion;
+      });
   }
 
 }
